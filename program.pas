0a8acad1606d86625f65f6105119eed629cf08b4
IgnacioN99 <inifl99@gmail.com>
2018-06-24 17:25:54 -0300

    LittleArreglos

--- program.pas
+++ program.pas
@@ -1,3 +1,16 @@
 program prueba;
+type
+  producto=record
+    cod:1..30;
+    cantVendida:integer;
+    costo:integer;
+    precioDeVenta:integer;
+  end;
+  productosEnVenta=array [1..10] of producto;
+  sucursal:record
+    cod:integer;
+    prod:productosEnVenta;
+    
+  end;
 begin
 end.
